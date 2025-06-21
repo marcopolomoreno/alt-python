138e34180060dc4e3614fccfd4bcd15f14be4deb
marcopolomoreno <marcopolo@unir.br>
2025-06-21 10:21:35 -0400

    Contagem de frases.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from contagem.letras import contar_letras
 from contagem.palavras import contar_palavras
 from contagem.palavrasComplexas import contar_palavras_complexas, carregar_banco_palavras
+from contagem.frases import contar_frases
 
 def ler_arquivo_texto(caminho='texto.txt'):
     try:
@@ -21,8 +22,22 @@ if __name__ == "__main__":
         print(f"Total de palavras: {total_palavras}")
 
 
+# Contagem de palavras complexas
 texto = open('texto.txt', 'r', encoding='utf-8').read()
 banco = carregar_banco_palavras()
 complexas = contar_palavras_complexas(texto, banco)
 
 print(f"Palavras complexas: {complexas}")
+
+
+# Contagem de frases com opções de ponto e vírgula e dois pontos
+chk_ponto_e_virgula = True
+chk_dois_pontos = True
+
+total_frases = contar_frases(
+    texto,
+    considerar_ponto_e_virgula=chk_ponto_e_virgula,
+    considerar_dois_pontos=chk_dois_pontos
+)
+
+print(f"Total de frases: {total_frases}")
